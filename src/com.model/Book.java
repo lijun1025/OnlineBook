6966688065f546801661f73c3586a3d629c25153
lijun1025 <1025429292@qq.com>
2017-01-08 12:00:02 +0800

    Update and rename LICENSE.md to src/com.model/Book.java

---
+++ src/com.model/Book.java
@@ -0,0 +1,26 @@
+public class Book{
+
+  private  String author;
+  private  double price;
+  private  String bookname;
+public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getBookname() {
+		return bookname;
+	}
+	public void setBookname(String bookname) {
+		this.bookname = bookname;
+	}
+	  
+	  
+}
